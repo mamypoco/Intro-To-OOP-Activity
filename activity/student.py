b6135c8c4ba4a0390f06f0e2769bb4519e1fbfb9
mami <mamypoco@gmail.com>
2025-09-25 11:52:31 -0700

    signed off

--- activity/student.py
+++ activity/student.py
@@ -1,1 +1,19 @@
 # add your Student class here!
+
+class Student:
+    def __init__(self, name, year, classes_list):
+        self.name = name
+        self.year = year
+        self.classes_list = classes_list
+    
+    def add_class(self, class_to_add):
+        self.classes_list.append(class_to_add)
+        return self.classes_list
+
+    def get_num_classes(self):
+        return len(self.classes_list)
+
+    def summary(self):
+        return f"{self.name} is a {self.year} enrolled in {self.get_num_classes()} classes"
+
+    
